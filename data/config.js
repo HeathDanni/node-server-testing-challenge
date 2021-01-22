7c4f672bd53512c8d1e495fa8243fe150f6d7ab8
Heather Beckman <heatherbeckman85@gmail.com>
2021-01-21 21:13:57 -0700

    writing tests

--- data/config.js
+++ data/config.js
@@ -1,4 +1,4 @@
 const knex = require("knex")
-const knexfile = require("./knexfile")
+const knexfile = require("../knexfile")
 
-module.exports= knex(knexfile[process.env.NODE.ENV])
+module.exports = knex(knexfile[process.env.NODE.ENV])
